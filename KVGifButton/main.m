5bf2552cc23f3f2e93f51db2a1943350e7c446e4
Vlad Kosyi <vladkosyi@Vlads-Mac-mini.local>
2016-05-10 13:02:19 +0300

    Add project

---
+++ KVGifButton/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KVGifButton
+//
+//  Created by Vlad Kosyi on 5/10/16.
+//  Copyright © 2016 Vlad Kosyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
